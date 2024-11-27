a8b05ea35510ee56c6001754db1eb9938961c159
Hovhannisyan111 <ogannisyan.2004.01@gmail.com>
2024-11-27 09:49:44 +0400

    Cleaning

--- index.js
+++
@@ -1,7 +0,0 @@
-function add(a, b) {
-  return a + b;
-}
-
-// Export the function for testing
-module.exports = { add };
-
--- test.js
+++
@@ -1,9 +0,0 @@
-const { add } = require('./index');
-
-// Simple test for the add function
-if (add(2, 3) === 5) {
-  console.log('Test passed: add(2, 3) === 5');
-} else {
-  console.error('Test failed: add(2, 3) !== 5');
-}
-
